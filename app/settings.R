c7ee42585e8bf1603724c334c5873fa122efa016
richarddmorey <richarddmorey@gmail.com>
2017-10-11 14:59:08 +0100

    various fixes, tidying up; broader browser compatibility

--- app/settings.R
+++ app/settings.R
@@ -27,7 +27,27 @@ par.list2 = par.list
 par.list2[['mar']] = c(4.5,1,1,1)
 
 getAssignments <- function(){
-  dir("assignments/", no..=TRUE)
+  a = dir("assignments/", no..=TRUE)
+  meta_info = sapply(a, function(d){
+    mpath = file.path("assignments",d,"meta_info.csv")
+    if(file.exists(mpath)){
+      meta_info0 = read.csv(mpath, header = TRUE, stringsAsFactors = FALSE)[1,]
+      meta_info = unlist(meta_info0)
+      names(meta_info) = names(meta_info0)
+    }else{
+      meta_info = c()
+    }
+    meta_info["title"] = ifelse(is.null(meta_info["title"]) || is.null(meta_info["title"]),
+                                d,
+                                meta_info["title"])
+    meta_info["sortkey"] = ifelse(is.null(meta_info["sortkey"]) || is.na(meta_info["sortkey"]),
+                                d,
+                                meta_info["sortkey"])
+    meta_info
+  })
+  names(a) = meta_info["title",]
+  ord = stringi::stri_order(meta_info["sortkey",], locale="en_US")
+  a[ord]
 }
 
 
@@ -41,6 +61,9 @@ set.seed.alpha <- function(x) {
 
 getURIdata<-function(seed, secret, format, assignment_list){
   myData = assignment_list$getData(seed, secret, assignment_list$assignment)
+  if(is.null(myData)){
+    return("")
+  }
   if(!is.data.frame(myData)){
     if(is.list(myData) & is.data.frame(myData[['data']])){
       myData = myData[['data']] 
@@ -48,41 +71,36 @@ getURIdata<-function(seed, secret, format, assignment_list){
       stop("Assignment configuration error. Data is not in correct format.")   
     }
   }
+  name_prefix = paste0("data_", assignment_list$assignment,"_")
   if(format=="SPSS"){
     ext="sav"
-    filenameWithExt = tempfile("data_",fileext=paste0(".",ext))
+    filenameWithExt = tempfile(name_prefix,fileext=paste0(".",ext))
     write_sav(myData, path=filenameWithExt)
   }else if(format=="Excel"){
     ext="xlsx"
-    filenameWithExt = tempfile("data_",fileext=paste0(".",ext))
+    filenameWithExt = tempfile(name_prefix,fileext=paste0(".",ext))
     write.xlsx(myData, file=filenameWithExt)
   }else if(format=="R data"){
     ext="Rda"
-    filenameWithExt = tempfile("data_",fileext=paste0(".",ext))
+    filenameWithExt = tempfile(name_prefix,fileext=paste0(".",ext))
     save(myData,file = filenameWithExt)
-    #write.xlsx(myData, file=filenameWithExt)
   }else{
     ext="csv"
-    filenameWithExt = tempfile("data_",fileext=paste0(".",ext))
+    filenameWithExt = tempfile(name_prefix,fileext=paste0(".",ext))
     write.csv(myData, file=filenameWithExt)
   }
-   divname = "dl.data.file"
+  divname = "dl.data.file"
   textHTML = "Click here to download the data."
-  
+  onlyFileName = basename(filenameWithExt)
   
   uri = dataURI(file = filenameWithExt, mime = "application/octet-stream", encoding = "base64")
-  paste0("<a style='text-decoration: none' id='",divname,"'></a>
+  paste0("<a style='text-decoration: none; cursor: pointer;' id='",divname,"'></a>
     <script>
-      var a = document.createElement('a');
+      var my_uri = '",uri,"';
+      var my_blob = dataURItoBlob(my_uri);
       var div = document.getElementById('",divname,"');
-      div.appendChild(a);
-      a.setAttribute('href', '",uri,"');
-      a.innerHTML = '",textHTML,"';
-      if (typeof a.download != 'undefined') {
-      a.setAttribute('download', '",filenameWithExt,"');
-      }else{
-      a.setAttribute('onclick', 'confirm(\"Your browser does not support the download HTML5 attribute. The file may be corrupted. Please use Chrome/Firefox/Opera to download the file. \")');
-      }
+      div.innerHTML = '",textHTML,"';
+      div.setAttribute('onclick', 'saveAs(my_blob, \"",onlyFileName ,"\");');
       </script>")
 }
 
@@ -108,17 +126,26 @@ assignment_time = function(assignmentDir, secret, tz = "Europe/London"){
   
   can_do = all(check.dates)
   if(!can_do){
-    stop("You cannot access this resource at this time.")
+    stop(safeError("You cannot access this resource at this time."))
   }
   return(can_do)
 }
 
 writeHeaders = function( file ){
   
+  require(htmltools)
+  
   assignments = getAssignments()
   html.content = NULL
   
-  allTags = htmltools::tagList()
+  allTags = tagList()
+  
+  # top js directory
+  fs = dir(paste0("js/"), full.names = TRUE)
+  for(f in fs){
+    lns = paste(readLines(f),collapse="\n")
+    allTags = tagAppendChild( allTags, tags$script(HTML(lns), type="text/javascript") )
+  }
   
   for(a in assignments){
     
@@ -126,14 +153,14 @@ writeHeaders = function( file ){
     fs = dir(paste0("assignments/",a,"/include/css/"), full.names = TRUE)
     for(f in fs){
       lns = paste(readLines(f),collapse="\n")
-      allTags = htmltools::tagAppendChild( allTags, htmltools::tags$style(lns,type="text/css") )
+      allTags = tagAppendChild( allTags, tags$style(HTML(lns),type="text/css") )
     }
     
     # JS
     fs = dir(paste0("assignments/",a,"/include/js/"), full.names = TRUE)
     for(f in fs){
       lns = paste(readLines(f),collapse="\n")
-      allTags = htmltools::tagAppendChild( allTags, htmltools::tags$script(lns, type="text/javascript") )
+      allTags = tagAppendChild( allTags, tags$script(HTML(lns), type="text/javascript") )
     }    
     
     # HTML
