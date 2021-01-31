3505ab6aec3aa34a0d78134fc1772bb93dacec22
Richard Morey <richarddmorey@gmail.com>
2021-01-31 20:10:45 +0000

    add assignment selector

--- R/getAssignments.R
+++ R/getAssignments.R
@@ -5,7 +5,7 @@
 #'
 #' @return
 #'
-getAssignments = function(path = system.file("assignments", package = "flexTeaching")){
+getAssignments = function(path = system.file("assignments", package = "flexTeaching"), simple = TRUE){
   potential_dirs = list.dirs(path, recursive = FALSE) 
   lapply(potential_dirs, function(d){
     fp = file.path(d,"_assignment.yml")
@@ -17,4 +17,20 @@ getAssignments = function(path = system.file("assignments", package = "flexTeach
   }) -> dirs
   names(dirs) = potential_dirs
   dirs[lengths(dirs) != 0]
+  
+  shortnames = sapply(dirs, function(el) el$shortname)
+  titles = sapply(dirs, function(el) el$title)
+  
+  if(simple){
+    names(shortnames) = titles
+    return(shortnames)
+  }
+  
+  for(i in seq_along(dirs)){
+    dirs[[i]]$path = names(dirs)[i] 
+  }
+  names(dirs) = shortnames
+
+  return(dirs)
+  
 }
