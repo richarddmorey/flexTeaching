70488ce43262f87415bbe456d17be774c84fbb52
Richard Morey <richarddmorey@gmail.com>
2021-02-01 22:21:01 +0000

    caching

--- R/launchApp.R
+++ R/launchApp.R
@@ -12,12 +12,16 @@
 #' @importFrom utils browseURL
 #' 
 #'
-launchApp <- function(which = "solve", query = ""){
+launchApp <- function(which = "solve", query = "", port = NULL){
   path = system.file(glue::glue("app/{which}.Rmd"), package = "flexTeaching")
   if(!file.exists(path))
     stop("Rmd file does not exist: ", path)
   rmarkdown::run(path,
-                 shiny_args = list(launch.browser = function(url){
-                   utils::browseURL(glue::glue("{url}/{which}.Rmd{query}"))
-                 }))
+                 shiny_args = list(
+                   launch.browser = function(url){
+                     utils::browseURL(glue::glue("{url}/{which}.Rmd{query}"))
+                     },
+                   port = port
+                   )
+                 )
 }
