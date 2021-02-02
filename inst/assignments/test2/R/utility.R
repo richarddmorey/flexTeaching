de3a2a70d1967b23053627a49c5ecd190d782bd3
Richard Morey <richarddmorey@gmail.com>
2021-02-02 22:54:20 +0000

    added PDF download

--- inst/assignments/test2/R/utility.R
+++ inst/assignments/test2/R/utility.R
@@ -9,10 +9,18 @@ get_data <- function(envir){
   assign(".flexteach_data", df, envir = envir)
 }
 
-compile_assignment_html <- function(path, envir = new.env()){
+compile_assignment_html <- function(path, envir = new.env(), ...){
   tmpfn = tempfile(fileext = ".html")
   input = file.path(path,"index.Rmd")
   output_format = rmarkdown::html_fragment(pandoc_args = c("--metadata", "title= " ) )
-  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, envir = envir, quiet = TRUE)
+  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, envir = envir, quiet = TRUE, ...)
+  return(tmpfn)
+}
+
+compile_assignment_pdf <- function(path, envir = new.env(), ...){
+  tmpfn = tempfile(fileext = ".pdf")
+  input = file.path(path,"index.Rmd")
+  output_format = "pdf_document"
+  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, envir = envir, quiet = TRUE, ...)
   return(tmpfn)
 }
