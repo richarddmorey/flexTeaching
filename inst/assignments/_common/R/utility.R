f078fae5c21c6474e3b6bd7c80fbf19c8a763120
Richard Morey <richarddmorey@gmail.com>
2021-02-03 22:10:32 +0000

    basic test assignment

--- inst/assignments/_common/R/utility.R
+++ inst/assignments/_common/R/utility.R
@@ -3,7 +3,8 @@ compile_assignment_html <- function(path, envir = new.env(), ...){
   tmpfn = tempfile(fileext = ".html")
   input = file.path(path, "index.Rmd")
   output_format = rmarkdown::html_fragment(pandoc_args = c("--metadata", "title= " ) )
-  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, envir = envir, quiet = TRUE, ...)
+  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, 
+                    envir = envir, quiet = TRUE, ...)
   return(tmpfn)
 }
 
@@ -11,6 +12,7 @@ compile_assignment_pdf <- function(path, envir = new.env(), ...){
   tmpfn = tempfile(fileext = ".pdf")
   input = file.path(path, "index.Rmd")
   output_format = "pdf_document"
-  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn, envir = envir, quiet = TRUE, ...)
+  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn,
+                    envir = envir, quiet = TRUE, ...)
   return(tmpfn)
 }
