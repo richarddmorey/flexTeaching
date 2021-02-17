2624cfe594629cc5ea591490b5d6082d096c2473
Richard Morey <richarddmorey@gmail.com>
2021-02-17 18:34:42 +0000

    sorting and categorizing of assignments

--- R/getAssignments.R
+++ R/getAssignments.R
@@ -4,6 +4,9 @@
 #' @param path Directory path
 #' @param simple return only a named vector (for selectInput) 
 #'
+#' @importFrom yaml read_yaml
+#' @importFrom purrr map_df map set_names
+#' @importFrom dplyr `%>%` bind_rows pull arrange select
 #' @return
 #'
 getAssignments = function(path = system.file("assignments", package = "flexTeaching"), simple = TRUE){
@@ -11,29 +14,33 @@ getAssignments = function(path = system.file("assignments", package = "flexTeach
   potential_dirs = list.dirs(path, recursive = FALSE) 
   potential_dirs = potential_dirs[ grepl("^[^_]", basename(potential_dirs)) ] 
   
-  lapply(potential_dirs, function(d){
-    fp = file.path(d,"_assignment.yml")
-    if(file.exists(fp)){
-      return(yaml::read_yaml(fp))
-    }else{
-      return(NULL)
-    }
-  }) -> dirs
-  names(dirs) = potential_dirs
-  dirs[lengths(dirs) != 0]
+  potential_dirs %>%
+    purrr::map(function(d){
+      fp = file.path(d,"_assignment.yml")
+      if(file.exists(fp)){
+        y = yaml::read_yaml(fp)
+        y$path = d
+        return(y)
+      }else{
+        return(NULL)
+      }
+    }) %>%
+    Filter(length, .) -> dirs
   
-  shortnames = sapply(dirs, function(el) el$shortname)
-  titles = sapply(dirs, function(el) el$title)
+  names(dirs) = purrr::map_chr(dirs, ~`$`(., 'shortname'))
+  if(!simple) return(dirs)
   
-  if(simple){
-    names(shortnames) = titles
-    return(shortnames)
-  }
+  purrr::map_df(dirs, function(el){
+    bind_rows(category = el$category, title = el$title, shortname = el$shortname, sortkey = el$sortkey)
+  }) %>% 
+    arrange(category, sortkey, title) %>%
+    select(-sortkey) %>%
+    split(.$category) %>%
+    purrr::map(function(el){
+      z = pull(el, shortname)
+      names(z) = pull(el, title)
+      return(z)
+    }) -> x
+  return(x)
   
-  for(i in seq_along(dirs)){
-    dirs[[i]]$path = names(dirs)[i] 
-  }
-  names(dirs) = shortnames
-
-  return(dirs)
 }
