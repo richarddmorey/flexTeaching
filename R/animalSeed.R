2eeade63ee9efe2bdbce2a82bcf48e74f62cc25e
Richard Morey <richarddmorey@gmail.com>
2021-07-21 13:46:08 +0100

    Changes to allow minimum yaml config for assignments

--- R/animalSeed.R
+++ R/animalSeed.R
@@ -2,22 +2,25 @@
 #'
 #' @param seed 
 #'
-#' @importFrom digest digest
+#' @importFrom digest sha1
+#' @importFRom dplyr `%>%`
 #' @importFrom R.utils withSeed
 #' @importFrom noah pseudonymize
 #' @return
 #'
 animalSeed <- function(seed, salt){
-  seed = trimws(seed)
-  salt = trimws(salt)
-  hash = digest::digest(paste0(seed, salt))
+  c(seed, salt) %>%
+    trimws() %>%
+    digest::sha1() %>%
+    alp2int() -> int_hash
+  
   animals = R.utils::withSeed({noah::pseudonymize("")},
-                              alp2int(hash))
+                              int_hash)
   animals = gsub(pattern = " ",
                  replacement = "_",
                  x = tolower(animals)
                  )
   alpha_num = R.utils::withSeed({randomAlphaNum(1)},
-                                alp2int(hash))
+                                int_hash)
   return(paste(animals, alpha_num,  sep = "_"))
 }
