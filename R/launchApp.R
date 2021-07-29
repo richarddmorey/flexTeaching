3277df85d43ad554e0a14820a1c215c37feb843d
Richard Morey <richarddmorey@gmail.com>
2021-07-29 12:47:25 +0100

    fix launchApp (bug and new single app)

--- R/launchApp.R
+++ R/launchApp.R
@@ -3,7 +3,7 @@
 #' Run one of the shiny interfaces
 #'
 #' @param which Which app to open ('solve' or 'download')
-#' @param query Query string to add to the URL
+#' @param query Query string to add to the URL (no initial '?')
 #'
 #' @return
 #' @export
@@ -13,13 +13,27 @@
 #' 
 #'
 launchApp <- function(which = "solve", query = "", port = NULL){
-  path = system.file(glue::glue("app/{which}.Rmd"), package = "flexTeaching")
+  if(which == "solve" & nchar(query) > 0){
+    query = glue::glue("solve&{query}")
+  }else if(which == "solve"){
+    query = "solve"
+  }
+  path  = system.file("app/app.Rmd", package = "flexTeaching")
   if(!file.exists(path))
     stop("Rmd file does not exist: ", path)
+  
+  # Reset working directory on exit
+  # Necessary because rmarkdown::run
+  # seems to use the current working 
+  # directory to serve the app
+  curwd = getwd()
+  on.exit( { setwd(curwd) } )
+  setwd(dirname(path))
+  
   rmarkdown::run(path,
                  shiny_args = list(
                    launch.browser = function(url){
-                     utils::browseURL(glue::glue("{url}/{which}.Rmd{query}"))
+                     utils::browseURL(glue::glue("{url}/app.Rmd?{query}"))
                      },
                    port = port
                    )
