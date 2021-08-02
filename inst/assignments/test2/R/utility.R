9fc0ba9dffa2172dcba01103197d0bed8badbb7b
Richard Morey <richarddmorey@gmail.com>
2021-08-02 12:52:25 +0100

    pdf creation function fix

--- inst/assignments/test2/R/utility.R
+++ inst/assignments/test2/R/utility.R
@@ -19,25 +19,66 @@ init <- function(assignment_data, id, seed, solutions, e){
 
 create_pdf <- function(assignment_data, id, seed, solutions, format, init, entry){
   
+  .DEBUG = FALSE
+  
   e = new.env(parent = .GlobalEnv)
   flexTeaching:::sourceAll(assignment_data, e)
   
   e$.flexteach_solutions = solutions
   e$.flexteach_info = init
   
-  tmpfn = tempfile(fileext = ".pdf")
-  input = file.path(assignment_data$path, "index.Rmd")
-  output_format = "pdf_document"
-  rmarkdown::render(input = input, output_format = output_format, output_file = tmpfn,
-                    intermediates_dir = dirname(tmpfn),
-                    envir = e, quiet = TRUE)
-  d = readBin(con = tmpfn, what = "raw", n = file.size(tmpfn))
-  time = format(Sys.time(), "%d%m%Y_%H%M%S")
-  fn = ifelse(entry=="solve",
-              glue::glue("{assignment_data$shortname}_{id}_{seed}_{time}.pdf"),
-              glue::glue("{assignment_data$shortname}_{id}_{time}.pdf")
+  dname = tempfile(pattern="d")
+  dir.create(dname)
+  
+  ## The following settings for tinytex.output_dir and tinytex.engine_args 
+  ## are is necessary due to a bug in rmarkdown where it doesn't properly
+  ## use intermediates_die for the extra tex-related files (log, etc)
+  ## See:  https://github.com/rstudio/rmarkdown/issues/1615 and
+  ##       https://github.com/rstudio/rmarkdown/issues/1975
+  withr::local_options(
+    list(
+      tinytex.clean = !.DEBUG,
+      tinytex.verbose = .DEBUG,
+      tinytex.output_dir = dname,
+      tinytex.engine_args = sprintf('"--output-directory=%s"', dname)
+    )
   )
-  return(list(fn = fn, d = d))
+  
+  tmpfn = tempfile(fileext = ".pdf", tmpdir = dname)
+  input = file.path(assignment_data$path, "index.Rmd")
+  output_format = rmarkdown::pdf_document(
+    keep_tex = .DEBUG, keep_md = .DEBUG
+  )
+  out0 = tryCatch(
+    {
+      rmarkdown::render(
+        input = input, output_format = output_format, output_file = tmpfn,
+        intermediates_dir = dname, envir = e, 
+        quiet = !.DEBUG, clean = !.DEBUG 
+      )
+      d = readBin(con = tmpfn, what = "raw", n = file.size(tmpfn))
+      time = format(Sys.time(), "%d%m%Y_%H%M%S")
+      fn = ifelse(entry=="solve",
+                  # Use sprintf instead of glue::glue to prevent code injection vulnerability
+                  sprintf("%s_practice_%s_%s_%s.pdf", assignment_data$shortname, id, seed, time),
+                  sprintf("%s_assignment_%s_%s.pdf", assignment_data$shortname, id, time)
+      )
+      return(list(fn = fn, d = d))
+    },
+    ## On error, pack up the whole folder and send it. This should probably
+    ## only be done while debugging, so I've started with a stop() call that
+    ## prevents it.
+    error = function(e){
+      if(!.DEBUG)
+        stop(e)
+      cat(file = file.path(dname,"paths.txt"),
+          glue::glue("dname: {dname}\ntmpfn: {tmpfn}\ntinytex.clean: {options()$`tinytex.clean`}\n{paste(e)}\nis_tinytex: {tinytex:::is_tinytex()}\ngetwd: {getwd()}\n"))
+      tmpzip = tempfile(pattern = glue::glue("PDF_ERROR_{basename(tmpfn)}_"), fileext = ".zip")
+      zip::zip(tmpzip, files = dir(dname, recursive = TRUE), root = dname)
+      d = readBin(con = tmpzip, what = "raw", n = file.size(tmpzip))
+      return(list(fn = basename(tmpzip), d = d))
+    }
+  )
 }
 
 buttons <- list(
