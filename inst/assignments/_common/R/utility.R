ad16a9c73ae1542f7ab02496aef52fa81d7e0a49
Richard D. Morey <richarddmorey@gmail.com>
2021-08-08 10:11:02 +0100

    change to glue_safe to avoid injection attacks

--- inst/assignments/_common/R/utility.R
+++ inst/assignments/_common/R/utility.R
@@ -4,9 +4,10 @@ data_file = function(assignment_data, id, seed, solutions, format, init, entry){
   hash = digest::digest(df)
   ext = flexTeaching:::formats[[format]]$ext
   time = format(Sys.time(), "%d%m%Y_%H%M%S")
+  shortname = assignment_data$shortname
   fn = ifelse(entry=="solve",
-              glue::glue("{entry}_{assignment_data$shortname}_{id}_{seed}_{time}_{hash}{ext}"),
-              glue::glue("{entry}_{assignment_data$shortname}_{id}_{time}_{hash}{ext}")
+              glue::glue_safe("{entry}_{shortname}_{id}_{seed}_{time}_{hash}{ext}"),
+              glue::glue_safe("{entry}_{shortname}_{id}_{time}_{hash}{ext}")
               )
   tf = tempfile(fileext = ext)
   flexTeaching:::formats[[format]]$f(df, tf)
