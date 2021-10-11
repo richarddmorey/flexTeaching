fdc164b277f93893ee1a41027052f3299f866c43
Richard Morey <richarddmorey@gmail.com>
2021-10-11 12:06:26 +0100

    remove dependency on noah

--- R/animalSeed.R
+++ R/animalSeed.R
@@ -13,14 +13,18 @@ animalSeed <- function(seed, salt){
     trimws() %>%
     digest::sha1() %>%
     alp2int() -> int_hash
+
+  animals = R.utils::withSeed(
+    {
+      with(name_parts, 
+      {
+        tolower(paste(sample(adjectives, 1), sample(animals, 1), sep='_'))
+      })      
+    }, int_hash)
   
-  animals = R.utils::withSeed({noah::pseudonymize("")},
-                              int_hash)
-  animals = gsub(pattern = " ",
-                 replacement = "_",
-                 x = tolower(animals)
-                 )
   alpha_num = R.utils::withSeed({randomAlphaNum(1)},
                                 int_hash)
   return(paste(animals, alpha_num,  sep = "_"))
 }
+
+
